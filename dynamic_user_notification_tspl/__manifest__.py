70de0f6c1260cd5b3fecd0c497827768981a76c9
Techno Stellar <technostellar03@gmail.com>
2026-04-01 22:51:19 +0530

    [ADD] Added 'dynamic_user_notification_tspl' v19 module

---
+++ dynamic_user_notification_tspl/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    # App information
+    'name': 'Dynamic User Notification',
+    'version': '19.0.1.0.0',
+    'summary': 'Configurable real-time backend notifications for Odoo users.(Dynamic Notification)',
+    'description': 'Dynamic User Notification adds a flexible real-time notification system for Odoo backend users. It lets administrators configure model-based notification rules for record creation, state-based updates, and deletion, so internal users receive instant alerts without custom development (Odoo user notifications, backend notifications, real-time alerts, Odoo notification engine, model-based notifications, record create alerts, state change notifications, record deletion alerts, internal user alerts, configurable notifications, Odoo bus notifications, admin notification rules, sticky notifications, workflow alerts, Odoo backend alerts).',
+    'category': 'Tools',
+    'license': 'LGPL-3',
+
+    # Author
+    'author': 'Techno Stellar',
+    'maintainer': 'Techno Stellar',
+
+    # Dependencies
+    'depends': ['base', 'mail'],
+
+    # Views & Data
+    'data': [
+        'security/ir.model.access.csv',
+        'views/notification_config_views.xml',
+        'views/menus.xml',
+    ],
+
+    # Technical
+    'images': ['static/description/banner.png'],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ dynamic_user_notification_tspl/globle.py
@@ -0,0 +1,3 @@
+class CommonVariables:
+    # Define your global variables here
+    operations = {'create': 'Created', 'write': 'Updated', 'unlink': 'Deleted'}
